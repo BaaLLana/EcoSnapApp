5024151bbeb021b55a8abcbd1ee6073814b01260
Muhamad Fauzi <115764458+Muhauzi@users.noreply.github.com>
2024-12-02 14:44:14 +0700

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const uploadRoutes = require('./routes/uploadRoutes');
 
 const init = async () => {
     const server = Hapi.server({
-        port: process.env.PORT || 3000, // Gunakan PORT dari environment atau fallback ke 3000
+        port: process.env.PORT || 8080, // Gunakan PORT dari environment atau fallback ke 8080
         host: '0.0.0.0', // Gunakan '0.0.0.0' agar bisa diakses secara publik
         routes: {
             cors: {
